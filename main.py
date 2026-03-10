3bb17419cbf201950d119c6d52d16dec20fa6470
Moroz-Evgeny <ya.moroz2020@gmail.com>
2026-03-11 00:03:19 +0300

    reload=false

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ app.add_middleware(
 )
  
 if __name__ == "__main__":
-  uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True, access_log=True)
+  uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=False, access_log=True)
